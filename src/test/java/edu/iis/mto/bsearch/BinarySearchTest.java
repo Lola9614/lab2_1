bb4cd6f5be0cadeface91ce1ea080dfc69bb6d4b
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-03-24 23:50:56 +0100

    test_v1 test cases signautres

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,39 @@
+package edu.iis.mto.bsearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    @Test
+    public void sequenceShouldHaveOneItemAndSearchElementIsInIt() {
+
+    }
+
+    @Test
+    public void sequenceShouldHaveOneItemAndSearchElementIsNotInIt(){
+
+    }
+
+    @Test
+    public void sequenceShouldHaveMoreThanOneItemAndSearchElementIsFirst(){
+
+    }
+
+    @Test
+    public void sequenceShouldHaveMoreThanOneItemAndSearchElementIsLast(){
+
+    }
+
+    @Test
+    public void sequenceShouldHaveMoreThanOneItemAndSearchElementIsInMiddle(){
+
+    }
+
+    @Test
+    public void sequneceShouldHaveMoreThanOneItemAndSearchElementIsNotInIt(){
+
+    }
+
+}
